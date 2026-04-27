1e4cf203636120155bea28ba68b7d506c9b9d5cb
Cauê da Rocha Garcia <cauerg7@gmail.com>
2026-04-27 13:24:49 -0300

    First commit.

---
+++ barber-pos-system-backend/src/main/java/br/com/barberflow/api/BarberPosSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.barberflow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberPosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberPosSystemApplication.class, args);
+	}
+
+}
