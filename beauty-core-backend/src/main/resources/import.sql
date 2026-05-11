9a4442b3eba7ef09daf763d82f8813fa2e2c7088
Cauê da Rocha Garcia <cauerg7@gmail.com>
2026-05-11 09:56:22 -0300

    import.sql updated.

--- beauty-core-backend/src/main/resources/import.sql
+++ beauty-core-backend/src/main/resources/import.sql
@@ -15,7 +15,7 @@ INSERT INTO professional_specialty (professional_id, specialty_id) VALUES (2, 2)
 INSERT INTO professional_specialty (professional_id, specialty_id) VALUES (3, 1);
 INSERT INTO professional_specialty (professional_id, specialty_id) VALUES (3, 2);
 
--- 3. SERVIÇOS (JobItems)
+-- 3. SERVIÇOS
 INSERT INTO services (id, name, base_price, created_at, updated_at) VALUES (nextval('sq_services'), 'Corte de Cabelo Masculino', 25.00, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 INSERT INTO services (id, name, base_price, created_at, updated_at) VALUES (nextval('sq_services'), 'Barba', 15.00, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 INSERT INTO services (id, name, base_price, created_at, updated_at) VALUES (nextval('sq_services'), 'Sobrancelha', 10.00, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
@@ -27,12 +27,15 @@ INSERT INTO clients (id, name, phone, credit, in_appointment, created_at, update
 INSERT INTO clients (id, name, phone, credit, in_appointment, created_at, updated_at) VALUES (nextval('sq_clients'), 'Lucas Oliveira', '71999881122', 0.00, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 -- 5. APPOINTMENTS
-INSERT INTO appointments (id, professional_id, client_id, appointment_status, created_at, finished_at) VALUES (nextval('sq_appointments'), 1, 1, 'FINISHED', '2026-05-04 10:00:00', '2026-05-04 10:45:00');
-INSERT INTO appointments (id, professional_id, client_id, appointment_status, created_at, finished_at) VALUES (nextval('sq_appointments'), 1, 2, 'FINISHED', '2026-05-04 11:00:00', '2026-05-04 12:00:00');
+INSERT INTO appointments (id, professional_id, client_id, appointment_status, discount, total_value, remaining_value, is_paid, created_at, finished_at) VALUES (nextval('sq_appointments'), 1, 1, 'FINISHED', 0.00, 35.00, 0.00, true, '2026-05-04 10:00:00', '2026-05-04 10:45:00');
+INSERT INTO appointments (id, professional_id, client_id, appointment_status, discount, total_value, remaining_value, is_paid, created_at, finished_at) VALUES (nextval('sq_appointments'), 2, 2, 'FINISHED', 0.00, 50.00, 0.00, true, '2026-05-04 11:00:00', '2026-05-04 12:00:00');
+INSERT INTO appointments (id, professional_id, client_id, appointment_status, discount, total_value, remaining_value, is_paid, created_at, finished_at) VALUES (nextval('sq_appointments'), 3, 2, 'FINISHED', 0.00, 50.00, 0.00, true, '2026-05-04 11:00:00', '2026-05-04 12:00:00');
 
-INSERT INTO appointment_service (appointment_id, service_id, price_at_moment, discount) VALUES (1, 1, 25, 5);
-INSERT INTO appointment_service (appointment_id, service_id, price_at_moment, discount) VALUES (1, 2, 15.00, 5);
+-- 6. APPOINTMENT_SERVICE
+INSERT INTO appointment_service (appointment_id, service_id, price_at_moment) VALUES (1, 1, 35.00);
+INSERT INTO appointment_service (appointment_id, service_id, price_at_moment) VALUES (1, 2, 35.00);
+INSERT INTO appointment_service (appointment_id, service_id, price_at_moment) VALUES (3, 2, 35.00);
 
--- 8. PAYMENTS
-INSERT INTO payments (id, appointment_id, payment_method, amount_paid, paid_at) VALUES (nextval('sq_payments'), 1, 'PIX', 60.00, '2026-05-04 10:45:00');
-INSERT INTO payments (id, appointment_id, payment_method, amount_paid, paid_at) VALUES (nextval('sq_payments'), 2, 'CASH', 50.00, '2026-05-04 12:00:00');
+-- 7. PAYMENTS
+INSERT INTO payments (id, appointment_id, payment_method, amount_paid, paid_at) VALUES (nextval('sq_payments'), 1, 'PIX', 35.00, '2026-05-04 10:45:00');
+INSERT INTO payments (id, appointment_id, payment_method, amount_paid, paid_at) VALUES (nextval('sq_payments'), 2, 'CASH', 35.00, '2026-05-04 12:00:00');
