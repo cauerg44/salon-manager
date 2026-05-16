1c082f949d6699d05f6451ecc4004a5b29d9424d
Cauê da Rocha Garcia <cauerg7@gmail.com>
2026-05-16 18:38:26 -0300

    wip: specializations page.

--- beauty-core-frontend/src/App.tsx
+++ beauty-core-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ClientHome from "./routes/ClientHome";
 import Home from "./routes/ClientHome/Home";
 import General from "./routes/ClientHome/General";
+import Specializations from "./routes/ClientHome/Specializations";
 
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
             <Route index element={<Home />} />
             <Route path="home" element={<Home />} />
             <Route path="general" element={<General />} />
+            <Route path="specializations" element={<Specializations />} />
           </Route>
           <Route path="*" element={<Navigate to={"/"} />} />
         </Routes>
---
+++ beauty-core-frontend/src/routes/ClientHome/Specializations/index.tsx
@@ -0,0 +1,12 @@
+
+import './styles.css';
+
+export default function Specializations() {
+  return (
+    <section id="specializations-section" className="bcf-container-1200px">
+      <div className='bcf-specialization-modal'>
+        <h2>Especialidades:</h2>
+      </div>
+    </section>
+  );
+}
