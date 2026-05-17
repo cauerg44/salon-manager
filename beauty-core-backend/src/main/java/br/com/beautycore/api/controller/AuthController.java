8c02688ac6ab4966b03ffde4fb34013d5020ee59
Cauê da Rocha Garcia <cauerg7@gmail.com>
2026-05-17 17:05:21 -0300

    feat: cors origin access backend.

--- beauty-core-backend/src/main/java/br/com/beautycore/api/controller/AuthController.java
+++ beauty-core-backend/src/main/java/br/com/beautycore/api/controller/AuthController.java
@@ -8,10 +8,7 @@ import br.com.beautycore.api.services.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/v1/auth")
