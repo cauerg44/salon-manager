b19d23b275d613a30eb305d4416cb46572495f4d
Cauê da Rocha Garcia <cauerg7@gmail.com>
2026-05-25 15:53:40 -0300

    feat: delete service item ok.

--- beauty-core-frontend/src/routes/ClientHome/Services/ServicesListing/index.tsx
+++ beauty-core-frontend/src/routes/ClientHome/Services/ServicesListing/index.tsx
@@ -1,10 +1,24 @@
-import { useEffect, useState } from 'react';
 import './styles.css';
+import { useEffect, useState } from 'react';
 import type { ServiceDTO } from '../../../../models/service-dto';
 import * as jobItemService from '../../../../services/job-item-service.ts';
-import ServiceCard from '../../../../components/ServiceCard/index.tsx';
+import editIcon from '../../../../assets/bcf-edit-icon.svg';
+import trashIcon from '../../../../assets/bcf-trash-icon.svg';
+import DialogConfirmation from '../../../../components/DialogConfirmation/index.tsx';
+import DialogModalInfo from '../../../../components/DialogInfo/index.tsx';
 
 export default function ServicesListing() {
+
+  const [dialogInfoData, setDialogInfoData] = useState({
+    visible: false,
+    message: "Operação com sucesso!"
+  })
+
+  const [dialogConfirmationData, setDialogConfirmationData] = useState({
+    visible: false,
+    id: 0,
+    message: "Tem certeza?"
+  })
 
   const [services, setServices] = useState<ServiceDTO[]>([]);
 
@@ -15,19 +29,92 @@ export default function ServicesListing() {
       })
   }, []);
 
+  function handleDeleteClick(serviceId: number) {
+    setDialogConfirmationData({ ...dialogConfirmationData, id: serviceId, visible: true });
+  }
+
+  function handleDialogInfoClose() {
+    setDialogInfoData({ ...dialogInfoData, visible: false });
+  }
+
+  function handleDialogConfirmationAnswer(answer: boolean, serviceId: number) {
+    if (answer) {
+      jobItemService.deleteServiceById(serviceId)
+        .then(() => {
+          setServices(services.filter(item => item.id !== serviceId));
+          setDialogInfoData({ ...dialogInfoData, message: 'Serviço removido com sucesso!', visible: true });
+        })
+        .catch(error => {
+          if (error.response.status === 403) {
+            setDialogInfoData({ ...dialogInfoData, message: 'Apenas o administrador tem acesso para remover este serviço do salão!', visible: true });
+            return;
+          }
+
+          setDialogInfoData({ ...dialogInfoData, message: error.response.data.error, visible: true });
+        });
+    }
+
+    setDialogConfirmationData({ ...dialogConfirmationData, visible: false });
+  }
+
   return (
-    <section id="services-listing-section" className="bcf-container-1200px">
+    <>
+      <section id="services-listing-section" className="bcf-container-1200px">
 
-      <h2 className='bcf-listing-services-title'>Listagem dos serviços disponíveis: </h2>
+        <h2 className='bcf-listing-services-title'>Listagem dos serviços disponíveis: </h2>
 
-      <div className='bcf-services-cards-modal'>
-        {
-          services.map(
-            serviceItem => <ServiceCard key={serviceItem.id} serviceDTO={serviceItem} />
-          )
-        }
-      </div>
+        <div className='bcf-services-cards-modal'>
+          {
+            services.map(
+              serviceItem =>
+                <div key={serviceItem.id} className='bcf-services-card-modal-container'>
+                  <div className='bcf-services-card-container'>
+                    <div>
+                      <h3>{serviceItem.name}</h3>
+                      <h4>R$ {serviceItem.basePrice.toFixed(2)}</h4>
+                    </div>
+                  </div>
 
-    </section>
+
+                  <div className='bcf-services-card-actions'>
+
+                    <div className='bcf-services-card-action-item'>
+                      <img src={editIcon} alt="Editar serviço" />
+                      <h5>Editar</h5>
+                    </div>
+
+                    <div onClick={() => handleDeleteClick(serviceItem.id)} className='bcf-services-card-action-item'>
+                      <img src={trashIcon} alt="Excluir serviço" />
+                      <h5>Excluir</h5>
+                    </div>
+
+                  </div>
+
+
+                </div>
+            )
+          }
+        </div>
+
+      </section>
+
+      {
+        dialogInfoData.visible &&
+        <DialogModalInfo
+          message={dialogInfoData.message}
+          onDialogClose={handleDialogInfoClose}
+        />
+      }
+
+      {
+        dialogConfirmationData.visible &&
+        <DialogConfirmation
+          id={dialogConfirmationData.id}
+          message={dialogConfirmationData.message}
+          onDialogAnswer={handleDialogConfirmationAnswer}
+        />
+      }
+
+    </>
   );
 }
--- beauty-core-frontend/src/services/job-item-service.ts
+++ beauty-core-frontend/src/services/job-item-service.ts
@@ -1,12 +1,22 @@
 import type { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
 
-
 export function findAllServices() {
 
   const config: AxiosRequestConfig = {
     url: "/services",
     withCredentials: true,
+  }
+
+  return requestBackend(config);
+}
+
+export function deleteServiceById(serviceId: number) {
+
+  const config: AxiosRequestConfig = {
+    method: "DELETE",
+    url: `/services/${serviceId}`,
+    withCredentials: true
   }
 
   return requestBackend(config);
